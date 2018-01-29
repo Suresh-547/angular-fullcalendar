7ba81816f5346fed8d672f5c8cfe1b1c07802862
Jitendra Kumar <jitendra@finoit.co.in>
2018-01-29 12:52:58 +0530

    calendar scheduler added

--- src/app/team-calendar/team-calendar.component.ts
+++ src/app/team-calendar/team-calendar.component.ts
@@ -5,6 +5,7 @@ import { CalendarComponent } from 'ng-fullcalendar';
 import { CalendarDialogComponent } from '../calendar-dialog/calendar-dialog.component';
 import { EventSesrvice } from '../event.service';
 import * as $ from 'jquery';
+import '../../assets/scheduler';
 
 @Component({
   selector: 'app-team-calendar',
@@ -13,203 +14,91 @@ import * as $ from 'jquery';
 })
 export class TeamCalendarComponent implements OnInit {
 
-    data = {};
-    selectedView = "m";
-    calendarOptions;
-    displayEvent: any;  
- 
+    public selectedView = 'm';
 
-    @ViewChild(CalendarComponent) ucCalendar: CalendarComponent;
+  constructor(
+  protected eventService: EventSesrvice,
+  public dialog: MatDialog,
+  private elRef: ElementRef) { }
 
-    constructor(
-      protected eventService: EventSesrvice,
-      public dialog: MatDialog,
-      private elRef:ElementRef) { }
+  ngOnInit() {
+    const calendar = (<any>$('#calendar'));
 
-    ngOnInit() {   
-
-      this.eventService.saveLocalStorage();
-      this.eventService.getEvents().subscribe(data => {
-        this.calendarOptions = {
-          editable: true,
-          eventLimit: false,
-          header: {
-            left: '',
-            center: 'title',
-            right: ''
-          },
-          events: data,
-          views: {
-              month: {
-                displayEventTime: false
-              }
+    calendar.fullCalendar({
+      now: '2017-12-07',
+      editable: true,
+      aspectRatio: 1.8,
+      scrollTime: '00:00',
+      header: {
+        left: 'promptResource today prev,next',
+        center: 'title',
+        right: 'timelineDay,timelineThreeDays,agendaWeek,month'
+      },
+      customButtons: {
+        promptResource: {
+          text: '+ room',
+          click: function() {
+            var title = prompt('Room name');
+            if (title) {
+              calendar.fullCalendar(
+                'addResource',
+                { title: title },
+                true // scroll to the new resource?
+              );
+            }
           }
-        };
-      });
-
-      setTimeout(()=> {
-        document.querySelector('tbody').addEventListener('click', this.addEvent.bind(this));
-      }, 500)
-    }
-
-    addEvent(){
-
-      let isOpened = document.getElementsByClassName('mat-dialog-container');
-      if (isOpened.length == 0) {
-        let data  = {
-            title: "", 
-            new: true,
-            startDate: {value: new Date()}, 
-            endDate: {value: new Date()}        
         }
-        this.openDialog(data);
-      }
-    }
+      },
+      defaultView: 'timelineDay',
+      views: {
+        timelineThreeDays: {
+          type: 'timeline',
+          duration: { days: 3 }
+        }
+      },
+      resourceLabelText: 'Rooms',
 
-
-  todayView() {
-    this.changeView(new Date());
-  }
-
-  changeView(date) {
-
-    let fullcalendar = (<any>$('ng-fullcalendar'));
-    switch (this.selectedView) {
-      case "d":
-        fullcalendar.fullCalendar('changeView', 'agendaDay', date);      
-        break;
-
-      case "w":
-        fullcalendar.fullCalendar('changeView', 'agendaWeek', date);      
-        break;  
-
-      case "m":
-        fullcalendar.fullCalendar('changeView', 'month', date);      
-        break;  
-
-      default:
-        alert("Please select view type")
-        break;
-    }
-  }
-
-  nextPre(a) {
-    let fullcalendar = (<any>$('ng-fullcalendar'));
-    switch (a) {
-      case "n":
-        fullcalendar.fullCalendar('next');
-        break;
-
-      case "p":
-        fullcalendar.fullCalendar('prev');
-        break;
-
-      default:
-        alert("Please select next or previous")
-        break;
-    }
-  }
-
-  eventClick(model: any) {
-
-      let _startDate, _endDate;
-
-      if (model.event.start) {_startDate = new FormControl(model.event.start._d)}
-
-      if (model.event.end) {
-        _endDate = new FormControl(model.event.end._d); 
-      }else{
-        _endDate = new FormControl(new Date(_startDate.value.getTime()));
-      }
-
-      model = {event: {id: model.event.id, start: model.event.start, startDate: _startDate, end: model.event.end, endDate: _endDate, title: model.event.title, allDay: model.event.allDay, type: model.event.type }, duration: {} }
-
-      this.displayEvent = model;
-      this.openDialog(model.event);
-
-  }
-
-
-  eventResize(model: any, type: string) {
-
-      model = this.initModel(model);
-      this.displayEvent = model;
-      let eventData = JSON.parse(localStorage.getItem('eventData'));
-
-      eventData.forEach((o) => {
-          if (o.id == model.event.id) {
-              let start, end, _duration = model.duration._data;
-                  start = o.start;
-                  end = this.updateDate(o.end, _duration);
-                  o.title = model.event.title;
-                  o.start = start;
-                  o.end = end;
-          }
-      });
-      localStorage.setItem('eventData', JSON.stringify(eventData));
-  }
-
-  updateEvent(model: any, type: string) {
-
-      model = this.initModel(model);
-
-      this.displayEvent = model;
-      let eventData = JSON.parse(localStorage.getItem('eventData'));
-
-      eventData.forEach((o) => {
-
-          if (o.id == model.event.id) {
-
-              let start, end, _duration = model.duration._data, _event = model.event;
-              if (!_event.end || _event.start._d.getTime() == _event.end._d.getTime()) {
-                  end = _event.start._d.setMinutes(_event.start._d.getMinutes()+30)
-              }
-                  start = this.updateDate(o.start, _duration);              
-                  end = this.updateDate(o.end, _duration);
-                  o.title = model.event.title;
-                  o.start = start;
-                  o.end = end;
-          }
-      });
-      localStorage.setItem('eventData', JSON.stringify(eventData));
-  }
-
-  updateDate(date, duration) {
-    let _date = new Date(date);
-    if (duration.milliseconds) _date.setMilliseconds(_date.getMilliseconds() + duration.milliseconds);
-    if (duration.seconds) _date.setSeconds(_date.getSeconds() + duration.seconds);
-    if (duration.minutes) _date.setMinutes(_date.getMinutes() + duration.minutes);
-    if (duration.hours) _date.setHours(_date.getHours() + duration.hours);
-    if (duration.days) _date.setDate(_date.getDate() + duration.days);
-    if (duration.months) _date.setMonth(_date.getMonth() + duration.months);
-
-    return _date;
-  }
-
-  initModel(model) {
-    model = {
-      event: {
-        id: model.event.id, 
-        start: model.event.start, 
-        end: model.event.end, 
-        title: model.event.title 
-      }, 
-      duration: {
-        _data: model.duration._data 
-      } 
-    };
-    return model
-  }
-
-  openDialog(model){
-    this.dialog.open(CalendarDialogComponent, {
-      data: model
-    });      
-  }
-  
-  clickButton(model: any) {
-    this.displayEvent = model;
-  }
-
-
-}
+      resources: [
+        { id: 'a', title: 'Auditorium A' },
+        { id: 'b', title: 'Auditorium B', eventColor: 'green' },
+        { id: 'c', title: 'Auditorium C', eventColor: 'orange' },
+        { id: 'd', title: 'Auditorium D', children: [
+          { id: 'd1', title: 'Room D1' },
+          { id: 'd2', title: 'Room D2' }
+        ] },
+        { id: 'e', title: 'Auditorium E' },
+        { id: 'f', title: 'Auditorium F', eventColor: 'red' },
+        { id: 'g', title: 'Auditorium G' },
+        { id: 'h', title: 'Auditorium H' },
+        { id: 'i', title: 'Auditorium I' },
+        { id: 'j', title: 'Auditorium J' },
+        { id: 'k', title: 'Auditorium K' },
+        { id: 'l', title: 'Auditorium L' },
+        { id: 'm', title: 'Auditorium M' },
+        { id: 'n', title: 'Auditorium N' },
+        { id: 'o', title: 'Auditorium O' },
+        { id: 'p', title: 'Auditorium P' },
+        { id: 'q', title: 'Auditorium Q' },
+        { id: 'r', title: 'Auditorium R' },
+        { id: 's', title: 'Auditorium S' },
+        { id: 't', title: 'Auditorium T' },
+        { id: 'u', title: 'Auditorium U' },
+        { id: 'v', title: 'Auditorium V' },
+        { id: 'w', title: 'Auditorium W' },
+        { id: 'x', title: 'Auditorium X' },
+        { id: 'y', title: 'Auditorium Y' },
+        { id: 'z', title: 'Auditorium Z' }
+      ],
+      events: [
+        { id: '1', resourceId: 'b', start: '2017-12-07T02:00:00', end: '2017-12-07T07:00:00', title: 'event 1' },
+        { id: '2', resourceId: 'c', start: '2017-12-07T05:00:00', end: '2017-12-07T22:00:00', title: 'event 2' },
+        { id: '3', resourceId: 'd', start: '2017-12-06', end: '2017-12-08', title: 'event 3' },
+        { id: '4', resourceId: 'e', start: '2017-12-07T03:00:00', end: '2017-12-07T08:00:00', title: 'event 4' },
+        { id: '5', resourceId: 'f', start: '2017-12-07T00:30:00', end: '2017-12-07T02:30:00', title: 'event 5' }
+      ]
+    });    
+    // setTimeout(() => {
+    //   console.log(calendar);
+    // }, 1000);
+  }    
+ }
